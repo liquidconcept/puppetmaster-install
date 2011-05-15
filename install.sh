0a8431093ea889519e11803701378faa261599f5
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-05-15 13:38:24 +0200

    add *** previous notice message to better show it

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # retstart if not root
 if [ "$USER" != 'root' ]; then
-  echo "You are not logged with root, login with root and restart the script"
+  echo "*** You are not logged with root, login with root and restart the script"
 
   # choose between local adn remote script
   script_path=$(cd ${0%/*} && echo $PWD/${0##*/})
@@ -15,21 +15,21 @@ if [ "$USER" != 'root' ]; then
 # run script if root
 else
 
-  echo "install git..."
+  echo "*** install git..."
   apt-get install -y git
 
-  echo "install puppet..."
+  echo "*** install puppet..."
   apt-get install -y puppet
 
-  echo "clone puppetmaster install repository"
+  echo "*** clone puppetmaster install repository"
   git clone https://github.com/liquidconcept/puppetmaster-install ~/puppetmaster-install
 
-  echo "run local puppet"
+  echo "*** run local puppet"
   module_path=$(cd ~/puppetmaster-install && echo $PWD/modules)
   script_path=$(cd ~/puppetmaster-install && echo $PWD/install.pp)
 
   puppet --modulepath "$module_path" $script_path
 
-  echo "remove puppetmaster install repository"
+  echo "*** remove puppetmaster install repository"
   rm -Rf ~/puppetmaster-install
 fi
