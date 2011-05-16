633baab06be7a4435108ee7a0cb408aefcb0015a
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-05-16 15:57:40 +0200

    run puppet agent

--- install.pp
+++ install.pp
@@ -3,3 +3,10 @@ Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" }
 package{"puppetmaster-passenger":
   ensure => present,
 }
+
+package{"puppet":
+  ensure => present,
+}
+
+exec{"puppet agent --server $fqdn":
+}
